f1c7881bb5efa6f0ace9e32e7c5e95a05adcc8d2
radeau <kevinradp@gmail.com>
2021-11-17 17:18:11 +0400

    started the gnl function

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,26 @@
 /*   By: kpoquita <kpoquita@42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 23:23:10 by kpoquita          #+#    #+#             */
-/*   Updated: 2021/11/16 23:23:38 by kpoquita         ###   ########.fr       */
+/*   Updated: 2021/11/17 17:16:05 by kpoquita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*gnl_line_read(char **buffer, char *gnl_ret_read, int fd)
+{
+	//variable for reading bytes,tmp var & newline
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[FD_MAX + 1];
+	char		*gnl_ret_read;
+	char		*gnl_res;
+
+	gnl_ret_read = (char *) malloc(sizeof(char) * BUFF_SIZE + 1);
+	if (!gnl_ret_read)
+		return NULL;
+	gnl_res = gnl_line_read(&buffer[fd], gnl_ret_read, fd);
+	return (gnl_res);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,19 @@
 /*   By: kpoquita <kpoquita@42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 23:23:54 by kpoquita          #+#    #+#             */
-/*   Updated: 2021/11/16 23:24:49 by kpoquita         ###   ########.fr       */
+/*   Updated: 2021/11/17 17:01:21 by kpoquita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFF_SIZE
+#  define BUFF_SIZE 1
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 256
+# endif
 
 #endif
